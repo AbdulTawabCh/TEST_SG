507773a715fec5d7bff3bbaa562e70caafa5c540
ATch <36518455+AbdulTawabCh@users.noreply.github.com>
2018-03-03 21:30:37 -0800

    added comments in php

--- Problem 2/index.php
+++ Problem 2/index.php
@@ -1,3 +1,33 @@
+//•	Problem 2: Implement a basic spin results end point 
+//o	Description 
+//		Slot Machine Spin Results is our server end point that updates all player data and features when a spin is completed on the client. 
+//		We do hundreds of millions of these requests per day, and we would like to see you make a very basic MySQL driven version.
+//		This can be just a normal PHP file that gets called, or you can implement more modern routing if you would like
+//o	Data Storage 
+//		Create a MySQL database that contains a player table with the following fields: 
+//		Player ID
+//		Name
+//		Credits
+//		Lifetime Spins
+//		Salt Value	
+//o	Code 
+//		Your code should validate the following request data: hash, coins won, coins bet, player ID
+//		Update the player data in MySQL if the data is valid
+//		Generate a JSON response with the following data: 
+//		Player ID
+//		Name
+//		Credits
+//		Lifetime Spins
+//		Lifetime Average Return
+//	
+//		You can assume that the client making the request has the salt value to make the hash.
+
+// Solution
+
+//	I am creating a hash using salt value along underscore, coins_won, coins_bet and player_id
+//	Salt_value = SG.is.Sciencific.Games
+//	hash = Salt_value + _ + coins_won + coins_bet + player_id
+
 		<?php
 			$response_data = array();
 			$response_data['success'] = false;
